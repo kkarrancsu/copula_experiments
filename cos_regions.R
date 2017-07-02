a253e9a9952af111583508bc88296d3bcf2443f3
Kiran <kiran.karra@gmail.com>
2017-07-02 17:43:28 -0400

    added further experiments

--- cos_regions.R
+++ cos_regions.R
@@ -1,7 +1,61 @@
+rm(list = ls())
+cat("\014")
+
+cosdv_regions_2d <- function(x, y) {
+  n=length(x)
+  vect <- order(x , y )
+  x1=x
+  y1=y
+  for ( i in 1:n) {
+    x1[i]=x[vect[i]]
+    y1[i]=y[vect[i]]
+  }
+  x=x1
+  y=y1
+  data<-list(u=x,v=y)
+  pseudodata <- sapply(data, rank, ties.method = "random") / (n+1 )
+  u=pseudodata[,1]
+  v=pseudodata[,2]
+  cop=u
+  for(i in 1:n) {
+    cpt=0
+    for(j in 1:i) {
+      if (( u[i]>=u[j]) && ( v[i]>=v[j])) {
+        cpt=cpt+1
+      }
+    }
+    cop[i]=cpt/(n + 1)
+  }
+  ss=cop[1]
+  
+  regionIdxVec <- vector()
+  regionIdx = 1
+  
+  for ( i in 2:n) {
+    ss=c(ss,cop[i])
+    if (( is.unsorted (ss))&&( is.unsorted (rev(ss)))) {
+      regionIdxVec[regionIdx] = i; 
+      regionIdx = regionIdx + 1;
+      
+      ss=cop[i]
+    }
+    else {
+      if (i==n) {
+        regionIdxVec[regionIdx] = i; 
+      }
+    }
+  }
+  
+  print(length(regionIdxVec))
+  print(regionIdxVec)
+  
+  return(cop)
+}
+
 cos_regions_3d <- function(x, y,z) {
   
   n=length(x)
-  vect <- order(x , y,z )
+  vect <- order(x,y,z )
   x1=x
   y1=y
   z1=z
@@ -23,6 +77,9 @@ cos_regions_3d <- function(x, y,z) {
   cop=u
   ss=cop[1]
   
+  regionIdxVec <- vector()
+  regionIdx = 1
+  
   for(i in 1:n) {
     cpt=0
     for(j in 1:i) {
@@ -36,20 +93,44 @@ cos_regions_3d <- function(x, y,z) {
   for ( i in 2:n) {
     ss=c(ss,cop[i])
     if (( is.unsorted (ss))&&( is.unsorted (rev(ss)))) {
-      print("new region IN BETWEEN")
+      
+      # print("new region IN BETWEEN")
+      regionIdxVec[regionIdx] = i; 
+      regionIdx = regionIdx + 1;
+      
       ss=cop[i]
     }
     else {
       if (i==n) {
-        print("new region at N!")
+        # print("new region at N!")
+        regionIdxVec[regionIdx] = i;
       }
     }
   }
+  
+  print(length(regionIdxVec))
+  return(cop)
 }
 
-n <- 500
-x = runif(n)
-y = runif(n)
-z = y*sin(x) - x*cos(y)
-cos_regions_3d(x,y,z)
-scatter3d(x,y,z)
+n <- 2500
+x = runif(n,-4,4)
+y = runif(n,-4,4)
+# z = y*sin(x) - x*cos(y)
+z = -x^2-y^2+6
+ecop_3d <- cos_regions_3d(x,y,z)
+
+x2 = runif(500)
+y2 = 4*(x2-0.5)^2
+ecop_2d <- cosdv_regions_2d(x2,y2)
+# plot(ecop_2d)
+
+par(mfrow=c(1,2))
+plot(ecop_2d)
+plot(ecop_3d)
+
+# plot3D::scatter3D(x,y,z)
+# plot(ecop)
+# scatterplot3d::scatterplot3d(x,y,z)
+
+# library("car")
+# scatter3d(x,y,z)
