5e1c7e2bb66b4f868415a1adf2ee5ed677a4cc9e
Kiran Karra <kiran.karra@gmail.com>
2017-09-13 22:59:26 -0400

    changed simulations

--- test_multivariate.m
+++ test_multivariate.m
@@ -7,53 +7,65 @@ clc;
 
 dVec = [2,3]; % the dimensionalities for which we will generate data
 numMCSims = 100;
-M = 300;
-copulasToTest = {'Clayton', 'Gumbel'};
-alphasToTest = linspace(1.5,30,10);
-numMetricsToCompute = 3;  % we compute Td, tau_d, and CoS
+M = 500;
+copulasToTest = {'Frank', 'Gumbel', 'Clayton'};
+numAlphaSteps = 10;
+alphasCell = {linspace(0.01,30,numAlphaSteps), ...
+              linspace(1,30,numAlphaSteps), ...
+              linspace(0.01,30,numAlphaSteps)};
+numMetricsToCompute = 5;  % we compute sRho1, sRho2, sRho3, sRho4, and CoS
 
-resultsVec = zeros(length(dVec),length(copulasToTest),length(alphasToTest),numMetricsToCompute);
+resultsVec = zeros(length(dVec),length(copulasToTest),numAlphaSteps,numMetricsToCompute);
+
+% Setup the Spearman's Rho's estimators
+mult = 1;
+ds_2d = ones(2,1); ds_3d = ones(3,1);
+dsCell = {ds_2d, ds_3d};
+
+s1_obj = ASpearman1_initialization(mult);
+s2_obj = ASpearman2_initialization(mult);
+s3_obj = ASpearman3_initialization(mult);
+s4_obj = ASpearman4_initialization(mult);
 
 dispstat('','init'); % One time only initialization
 dispstat(sprintf('Begining the simulation...\n'),'keepthis','timestamp');
 
 for dIdx=1:length(dVec)
     d = dVec(dIdx);
+    ds = dsCell{dIdx};
     dispstat(sprintf('Computing for d=%d',d),'keepthis', 'timestamp');
     for copulaIdx=1:length(copulasToTest)
         copulaToTest = copulasToTest{copulaIdx};
-        if(strcmpi(copulaToTest,'clayton'))
+        alphasToTest = alphasCell{copulaIdx};
+        if(strcmpi(copulaToTest,'frank'))
+            coprng = @frankcopularnd;
+        elseif(strcmpi(copulaToTest,'clayton'))
             coprng = @claytoncopularnd;
         elseif(strcmpi(copulaToTest,'gumbel'))
             coprng = @gumbelcopularnd;
         end
         for alphaIdx=1:length(alphasToTest)
             alphaVal = alphasToTest(alphaIdx);
-            T_d_sum = 0; tau_d_sum = 0; cos_sum = 0;
+            cos_sum = 0; srho1_sum = 0; srho2_sum = 0; srho3_sum = 0; srho4_sum = 0;
             for mcSimNum=1:numMCSims
                 U = coprng(M, d, alphaVal);
-                % compute T_d
-                if(d==2)
-                    T_d_sum = T_d_sum + corr(U(:,1),U(:,2),'type','kendall');
-                elseif(d==3)
-                    sumVal = corr(U(:,1),U(:,2),'type','kendall') + ...
-                             corr(U(:,1),U(:,3),'type','kendall') + ...
-                             corr(U(:,2),U(:,3),'type','kendall');
-                    T_d_sum = T_d_sum + 1/6*sumVal;
-                end
+                UU = U';  % the format needed by the ITE toolbox :/
                 
-                % compute tau_d
-                if(d==2)
-                    tau_d_sum = tau_d_sum + corr(U(:,1),U(:,2),'type','kendall');
-                elseif(d==3)
-                    % a vectorized but naive way to compute! -- research
-                    % better ways perhaps ...
-                    sumVal = 0;
-                    for ii=1:M
-                        sumVal = sumVal + sum(sum(U(ii,:)<U(ii+1:end,:),2)==d);
-                    end
-                    tau_d_sum = tau_d_sum + (2^d/(M*(M-1))*sumVal-1)*(1/(2^(d-1)-1));
-                end
+                % compute Spearman1
+                sRho1 = ASpearman1_estimation(UU,ds,s1_obj);
+                srho1_sum = srho1_sum + sRho1;
+                
+                % compute Spearman2
+                sRho2 = ASpearman2_estimation(UU,ds,s2_obj);
+                srho2_sum = srho2_sum + sRho2;
+                
+                % compute Spearman3
+                sRho3 = ASpearman3_estimation(UU,ds,s3_obj);
+                srho3_sum = srho3_sum + sRho3;
+                
+                % compute Spearman4
+                sRho4 = ASpearman4_estimation(UU,ds,s4_obj);
+                srho4_sum = srho4_sum + sRho4;
                 
                 % compute CoS
                 if(d==2)
@@ -63,9 +75,11 @@ for dIdx=1:length(dVec)
                 end
             end
             % store average results
-            resultsVec(dIdx,copulaIdx,alphaIdx,1) = T_d_sum/numMCSims;
-            resultsVec(dIdx,copulaIdx,alphaIdx,2) = tau_d_sum/numMCSims;
-            resultsVec(dIdx,copulaIdx,alphaIdx,3) = cos_sum/numMCSims;
+            resultsVec(dIdx,copulaIdx,alphaIdx,1) = srho1_sum/numMCSims;
+            resultsVec(dIdx,copulaIdx,alphaIdx,2) = srho2_sum/numMCSims;
+            resultsVec(dIdx,copulaIdx,alphaIdx,3) = srho3_sum/numMCSims;
+            resultsVec(dIdx,copulaIdx,alphaIdx,4) = srho4_sum/numMCSims;
+            resultsVec(dIdx,copulaIdx,alphaIdx,5) = cos_sum/numMCSims;
         end
     end
 end
@@ -88,46 +102,81 @@ elseif(isunix)
 end
 
 figure;
-subplot(2,2,1); 
+subplot(2,3,1); 
 copulaIdx = 1;
 dIdx = 1;
+alphasToTest = alphasCell{copulaIdx};
 plot(alphasToTest, squeeze(resultsVec(dIdx,copulaIdx,:,1)),'o-.'); hold on;
 plot(alphasToTest, squeeze(resultsVec(dIdx,copulaIdx,:,2)),'+-.');
 plot(alphasToTest, squeeze(resultsVec(dIdx,copulaIdx,:,3)),'d-.');
-title({copulasToTest{copulaIdx}, 'D=2'});
+plot(alphasToTest, squeeze(resultsVec(dIdx,copulaIdx,:,4)),'v-.');
+plot(alphasToTest, squeeze(resultsVec(dIdx,copulaIdx,:,5)),'s-.');
+title({copulasToTest{copulaIdx}, sprintf('D=%d',dVec(dIdx))});
 xlabel('\alpha');
-legend('T_d','\tau_d','CoS');
+legend('\rho_s_1','\rho_s_2','\rho_s_3','\rho_s_4','CoS', 'location', 'southeast');
 grid on;
 
-subplot(2,2,2); 
+subplot(2,3,2); 
 copulaIdx = 2;
 dIdx = 1;
+alphasToTest = alphasCell{copulaIdx};
 plot(alphasToTest, squeeze(resultsVec(dIdx,copulaIdx,:,1)),'o-.'); hold on;
 plot(alphasToTest, squeeze(resultsVec(dIdx,copulaIdx,:,2)),'+-.');
 plot(alphasToTest, squeeze(resultsVec(dIdx,copulaIdx,:,3)),'d-.');
-title({copulasToTest{copulaIdx}, 'D=2'});
+plot(alphasToTest, squeeze(resultsVec(dIdx,copulaIdx,:,4)),'v-.');
+plot(alphasToTest, squeeze(resultsVec(dIdx,copulaIdx,:,5)),'s-.');
+title({copulasToTest{copulaIdx}, sprintf('D=%d',dVec(dIdx))});
 xlabel('\alpha');
-legend('T_d','\tau_d','CoS');
 grid on;
 
-subplot(2,2,3);
+subplot(2,3,3); 
+copulaIdx = 3;
+dIdx = 1;
+alphasToTest = alphasCell{copulaIdx};
+plot(alphasToTest, squeeze(resultsVec(dIdx,copulaIdx,:,1)),'o-.'); hold on;
+plot(alphasToTest, squeeze(resultsVec(dIdx,copulaIdx,:,2)),'+-.');
+plot(alphasToTest, squeeze(resultsVec(dIdx,copulaIdx,:,3)),'d-.');
+plot(alphasToTest, squeeze(resultsVec(dIdx,copulaIdx,:,4)),'v-.');
+plot(alphasToTest, squeeze(resultsVec(dIdx,copulaIdx,:,5)),'s-.');
+title({copulasToTest{copulaIdx}, sprintf('D=%d',dVec(dIdx))});
+xlabel('\alpha');
+grid on;
+
+subplot(2,3,4); 
 copulaIdx = 1;
 dIdx = 2;
+alphasToTest = alphasCell{copulaIdx};
 plot(alphasToTest, squeeze(resultsVec(dIdx,copulaIdx,:,1)),'o-.'); hold on;
 plot(alphasToTest, squeeze(resultsVec(dIdx,copulaIdx,:,2)),'+-.');
 plot(alphasToTest, squeeze(resultsVec(dIdx,copulaIdx,:,3)),'d-.');
-title({copulasToTest{copulaIdx}, 'D=3'});
+plot(alphasToTest, squeeze(resultsVec(dIdx,copulaIdx,:,4)),'v-.');
+plot(alphasToTest, squeeze(resultsVec(dIdx,copulaIdx,:,5)),'s-.');
+title({copulasToTest{copulaIdx}, sprintf('D=%d',dVec(dIdx))});
 xlabel('\alpha');
-legend('T_d','\tau_d','CoS');
 grid on;
 
-subplot(2,2,4);
+subplot(2,3,5); 
 copulaIdx = 2;
 dIdx = 2;
+alphasToTest = alphasCell{copulaIdx};
 plot(alphasToTest, squeeze(resultsVec(dIdx,copulaIdx,:,1)),'o-.'); hold on;
 plot(alphasToTest, squeeze(resultsVec(dIdx,copulaIdx,:,2)),'+-.');
 plot(alphasToTest, squeeze(resultsVec(dIdx,copulaIdx,:,3)),'d-.');
-title({copulasToTest{copulaIdx}, 'D=3'});
+plot(alphasToTest, squeeze(resultsVec(dIdx,copulaIdx,:,4)),'v-.');
+plot(alphasToTest, squeeze(resultsVec(dIdx,copulaIdx,:,5)),'s-.');
+title({copulasToTest{copulaIdx}, sprintf('D=%d',dVec(dIdx))});
 xlabel('\alpha');
-legend('T_d','\tau_d','CoS');
+grid on;
+
+subplot(2,3,6); 
+copulaIdx = 3;
+dIdx = 2;
+alphasToTest = alphasCell{copulaIdx};
+plot(alphasToTest, squeeze(resultsVec(dIdx,copulaIdx,:,1)),'o-.'); hold on;
+plot(alphasToTest, squeeze(resultsVec(dIdx,copulaIdx,:,2)),'+-.');
+plot(alphasToTest, squeeze(resultsVec(dIdx,copulaIdx,:,3)),'d-.');
+plot(alphasToTest, squeeze(resultsVec(dIdx,copulaIdx,:,4)),'v-.');
+plot(alphasToTest, squeeze(resultsVec(dIdx,copulaIdx,:,5)),'s-.');
+title({copulasToTest{copulaIdx}, sprintf('D=%d',dVec(dIdx))});
+xlabel('\alpha');
 grid on;
